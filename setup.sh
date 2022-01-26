49f71352d08f7661fd1c21ac96ba4a1a5b11c45f
Felix Ghosh <zanzaniman@gmail.com>
2022-01-26 12:10:30 +0000

    updated setup to include installation of libs, and gitignore

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
   git config --global user.email "zanzaniman@gmail.com"
   git config --global user.name "Felix Ghosh"
+  wget http://tmrh20.github.io/RF24Installer/RPi/install.sh
+  chmod +x install.sh
+  ./install.sh
+  cd rf24libs/RF24/examples_linux
